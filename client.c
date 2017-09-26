a0a1bd705d2f50d2eece080bf88b87f8bc0a6aa0
Jesse Stanger <jesse.stanger@gmail.com>
2017-09-26 15:18:43 +1000

    Formatted in Google style

--- client.c
+++ client.c
@@ -1,121 +1,119 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <string.h> 
-#include <netdb.h> 
-#include <sys/types.h> 
-#include <netinet/in.h> 
-#include <sys/socket.h> 
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <windows.h>
 
-    #define MAXDATASIZE 100 /* max number of bytes we can get at once */
+#define MAXDATASIZE 100 /* max number of bytes we can get at once */
 
-	#define STRING_SIZE 7
-    // #define ARRAY_SIZE 30
+#define STRING_SIZE 7
+// #define ARRAY_SIZE 30
 
-	#define RETURNED_ERROR -1
+#define RETURNED_ERROR -1
 
+void PlayHangman(char *word) {
+    int guesses = min(strlen(word) + strlen(word) + 10, 26);
+    char *guessedLetters = malloc(guesses);
+    char *currentWord = malloc(strlen(word));
 
-void PlayHangman(char* word) {
-	int guesses = min(strlen(word) + strlen(word) + 10, 26);
-	char* guessedLetters = malloc(guesses);
-	char* currentWord = malloc(strlen(word));
+    printf("Guessed letters: \n\n");
+    printf("Number of guesses left: %d\n\n", guesses);
 
-	printf("Guessed letters: \n\n");
-	printf("Number of guesses left: %d\n\n", guesses);
-	
-	int i = 0;
-	//print the word
-	for (i = 0; i < strlen(word); i++) {
-		currentWord[i] = '_';
-		printf("%c ", currentWord[i]);
-	}
-	printf("\n\nEnter your guess - ");
+    int i = 0;
+    // print the word
+    for (i = 0; i < strlen(word); i++) {
+        currentWord[i] = '_';
+        printf("%c ", currentWord[i]);
+    }
+    printf("\n\nEnter your guess - ");
 
-	//while there are still guesses available
-	char c;
-	while(1) {
-		scanf("%c", &c);
-		if (c != '\n') {
-			if (isalpha(c)) {
-				printf("TESTING\n");
-			}
-		}
-	}
-
+    // while there are still guesses available
+    char c;
+    while (1) {
+        scanf("%c", &c);
+        if (c != '\n') {
+            if (isalpha(c)) {
+                printf("TESTING\n");
+            }
+        }
+    }
 }
 
 void Receive_Array_Int_Data(int socket_identifier, int size) {
-    int number_of_bytes, i=0;
+    int number_of_bytes, i = 0;
     uint8_t statistics;
-	char finalOutput[MAXDATASIZE] = "";
+    char finalOutput[MAXDATASIZE] = "";
 
-	char *results = malloc(sizeof(char)*STRING_SIZE);
+    char *results = malloc(sizeof(char) * STRING_SIZE);
 
-	for (i=0; i < size; i++) {
-		if ((number_of_bytes=recv(socket_identifier, &statistics, sizeof(uint8_t), 0)) == RETURNED_ERROR) {
-			perror("recv");
-			exit(EXIT_FAILURE);			
-		}
-		results[i] = statistics;
-	}
-	for (i=0; i < STRING_SIZE; i++) {
-		//THIS JOINS THE STRING TOGETHER
-		strncat(finalOutput, &results[i], 1);
-	}
+    for (i = 0; i < size; i++) {
+        if ((number_of_bytes = recv(socket_identifier, &statistics,
+                                    sizeof(uint8_t), 0)) == RETURNED_ERROR) {
+            perror("recv");
+            exit(EXIT_FAILURE);
+        }
+        results[i] = statistics;
+    }
+    for (i = 0; i < STRING_SIZE; i++) {
+        // THIS JOINS THE STRING TOGETHER
+        strncat(finalOutput, &results[i], 1);
+    }
 
-	printf("%s\n", finalOutput);
-	PlayHangman(finalOutput);
-	
+    printf("%s\n", finalOutput);
+    PlayHangman(finalOutput);
 }
 
 int main(int argc, char *argv[]) {
-    int sock_fd, numbytes, i=0;  
-	char buf[MAXDATASIZE];
-	struct hostent *he;
-	struct sockaddr_in serv_addr; /* connector's address information */
+    int sock_fd, numbytes, i = 0;
+    char buf[MAXDATASIZE];
+    struct hostent *he;
+    struct sockaddr_in serv_addr; /* connector's address information */
 
     if (argc != 3) {
-		fprintf(stderr,"usage: client_hostname port_number\n");
-		exit(1);
-	}
+        fprintf(stderr, "usage: client_hostname port_number\n");
+        exit(1);
+    }
 
-	if ((he=gethostbyname(argv[1])) == NULL) {  /* get the host info */
-		herror("gethostbyname");
-		exit(1);
-	}
+    if ((he = gethostbyname(argv[1])) == NULL) { /* get the host info */
+        herror("gethostbyname");
+        exit(1);
+    }
 
-	if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
 
-    serv_addr.sin_family = AF_INET;      /* host byte order */
-	serv_addr.sin_port = htons(atoi(argv[2]));    /* short, network byte order */
-	serv_addr.sin_addr = *((struct in_addr *)he->h_addr);
-    bzero(&(serv_addr.sin_zero), 8);     /* zero the rest of the struct */
+    serv_addr.sin_family = AF_INET;            /* host byte order */
+    serv_addr.sin_port = htons(atoi(argv[2])); /* short, network byte order */
+    serv_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    bzero(&(serv_addr.sin_zero), 8); /* zero the rest of the struct */
 
-    if (connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) == -1) {
-		perror("connect");
-		exit(1);
-	}
+    if (connect(sock_fd, (struct sockaddr *)&serv_addr,
+                sizeof(struct sockaddr)) == -1) {
+        perror("connect");
+        exit(1);
+    }
 
     /* Receive data from server */
-	Receive_Array_Int_Data(sock_fd, STRING_SIZE);
+    Receive_Array_Int_Data(sock_fd, STRING_SIZE);
 
     /* Receive message back from server */
-    if ((numbytes=recv(sock_fd, buf, MAXDATASIZE, 0)) == -1) {
-		perror("recv");
-		exit(1);
-	}
+    if ((numbytes = recv(sock_fd, buf, MAXDATASIZE, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
 
-	buf[numbytes] = '\0';
+    buf[numbytes] = '\0';
 
-	printf("Received: %s",buf);
+    printf("Received: %s", buf);
 
-	close(sock_fd);
+    close(sock_fd);
 
-	return 0;
-
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,20 +1,19 @@
 #include <arpa/inet.h>
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <netinet/in.h> 
-#include <sys/socket.h> 
-#include <sys/wait.h> 
-#include <unistd.h>
 #include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-    #define ARRAY_SIZE 30
+#define ARRAY_SIZE 30
 
-    #define BACKLOG 10     /* how many pending connections queue will hold */
+#define BACKLOG 10 /* how many pending connections queue will hold */
 
-    #define RETURNED_ERROR -1
+#define RETURNED_ERROR -1
 
 void Testing_Function(int socket_id) {
     char* testStr = "testing";
@@ -33,75 +32,83 @@ void Testing_Function(int socket_id) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char* argv[]) {
     /* Thread attributes here */
     pthread_t client_thread;
-	pthread_attr_t attr;
-    
+    pthread_attr_t attr;
+
     int sock_fd, new_fd; /* listen on sock_fd, new connection on new_fd */
-    struct sockaddr_in serv_addr; /* Server address info */
+    struct sockaddr_in serv_addr;   /* Server address info */
     struct sockaddr_in client_addr; /* Client address info */
     socklen_t sin_size;
     int i = 0;
-    
-    /* Get port number for server to listen on */
-	if (argc != 2) {
-		fprintf(stderr,"usage: client port_number\n");
-		exit(1);
-	}
 
-	/* generate the socket */
-	if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
+    /* Get port number for server to listen on */
+    if (argc != 2) {
+        fprintf(stderr, "usage: client port_number\n");
+        exit(1);
+    }
+
+    /* generate the socket */
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
 
     /* Generate socket */
-    //The function creates a socket with AF_INET ( IP Addressing ) and of type SOCK_STREAM. Data from all devices wishing to connect on this socket will be redirected to sock_fd
-    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { 
-		perror("socket");
-		exit(1);
-	}
-    
+    // The function creates a socket with AF_INET ( IP Addressing ) and of type
+    // SOCK_STREAM. Data from all devices wishing to connect on this socket will
+    // be redirected to sock_fd
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
     /* Generate end point */
-    serv_addr.sin_family = AF_INET;               /* host byte order */ 
-	serv_addr.sin_port = htons(atoi(argv[1]));    /* short, network byte order (IP put in my server when you first run) */ 
-	serv_addr.sin_addr.s_addr = INADDR_ANY;       /* auto-fill with my IP */
-    
+    serv_addr.sin_family = AF_INET; /* host byte order */
+    serv_addr.sin_port = htons(
+        atoi(argv[1])); /* short, network byte order (IP put in my server when
+                           you first run) */
+    serv_addr.sin_addr.s_addr = INADDR_ANY; /* auto-fill with my IP */
+
     /* Bind socket to end point */
-    if (bind(sock_fd, (struct sockaddr*)&serv_addr, sizeof(struct sockaddr)) == -1) {
-		perror("bind");
-		exit(1);
-	}
-    
+    if (bind(sock_fd, (struct sockaddr*)&serv_addr, sizeof(struct sockaddr)) ==
+        -1) {
+        perror("bind");
+        exit(1);
+    }
+
     /* Start listening */
-    //Start Listening for connections , keep at the most 10 connection requests waiting.If there are more than 10 computers wanting to connect at a time, the 11th one fails to.
+    // Start Listening for connections , keep at the most 10 connection requests
+    // waiting.If there are more than 10 computers wanting to connect at a time,
+    // the 11th one fails to.
     if (listen(sock_fd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
-    
+        perror("listen");
+        exit(1);
+    }
+
     /* Main accept() loop */
-    while(1) {
+    while (1) {
         sin_size = sizeof(struct sockaddr_in);
-        if ((new_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &sin_size)) == -1) {
+        if ((new_fd = accept(sock_fd, (struct sockaddr*)&client_addr,
+                             &sin_size)) == -1) {
             perror("accept");
             continue;
         }
-        printf("server: got connection from %s\n", inet_ntoa(client_addr.sin_addr));
-        
+        printf("server: got connection from %s\n",
+               inet_ntoa(client_addr.sin_addr));
+
         /* Create a thread to accept client */
-        
+
         pthread_attr_t attr;
-		pthread_attr_init(&attr);
-		pthread_create(&client_thread, &attr, Testing_Function, new_fd);
+        pthread_attr_init(&attr);
+        pthread_create(&client_thread, &attr, Testing_Function, new_fd);
 
-		pthread_join(client_thread,NULL);
+        pthread_join(client_thread, NULL);
 
-		if (send(new_fd, "All of array data sent by server\n", 40 , 0) == -1) {
+        if (send(new_fd, "All of array data sent by server\n", 40, 0) == -1) {
             perror("send");
         }
     }
-    close(new_fd);  
+    close(new_fd);
 }
