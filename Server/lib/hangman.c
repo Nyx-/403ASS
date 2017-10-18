c465cb534ba74e1ab7158d5279352b8eb1782d30
Nyx- <mnhunter@outlook.com>
2017-10-18 18:44:42 +1000

    Getting the words works now yay

--- Server/lib/hangman.c
+++ Server/lib/hangman.c
@@ -7,18 +7,12 @@
 #include "hangman.h"
 
 void gameSetup() {
-    printf("setting up game\n");
+    printf("Setting up game\n");
     Hangman *h = malloc(sizeof(Hangman));
 
     h->word_pair = (char **) selectWords();
     h->word1 = h->word_pair[0];
     h->word2 = h->word_pair[1];
-
-    printf("Word 1 = %s\n", h->word_pair[0]);
-    printf("Word 1 len = %d\n", (int)strlen(h->word_pair[0]));
-
-    printf("Word 2 = %s\n", h->word_pair[1]);
-    printf("Word 2 len = %d\n", (int)strlen(h->word_pair[1]));
 
     printf("Word 1 = %s\n", h->word1);
     printf("Word 2 = %s\n", h->word2);
@@ -26,14 +20,10 @@ void gameSetup() {
 
 void **selectWords() {
     int num = -1;
-
+    //Need to do something here cause the same number pops up each time
     num = rand() % controller->c_wordList->numWords;
+    printf("Word number: %d\n", num);
 
-    printf("Word 1 = %s\n", controller->c_wordList->words[num]);
-
-    printf("Number: %d\n", num);
-
-    
     return controller->c_wordList->words[num];
 }
 
@@ -48,11 +38,14 @@ WordList *createWordList() {
 
 void loadWords() {
     FILE *file = fopen("hangman_text.txt", "r");
-    const char sep1[2] = ",";
+    const char sep1[1] = ",";
     const char sep2[2] = "\n";
     char buffer[150];
     char *token;
     void **wordPair;
+    char *joinedWords;
+    char *word_1; 
+    char *word_2;
 
     if (file == NULL) {
         perror("Unable to read Hangman file");
@@ -60,32 +53,32 @@ void loadWords() {
     }
 
     while(fgets(buffer, sizeof(buffer), file) != NULL) {
-        int numWords = 2;
+        // int numWords = 2;
         wordPair = malloc(2 * sizeof(char*));
+        joinedWords = malloc(256 * sizeof(char));
+        word_1 = malloc(256 * sizeof(char));
+        word_2 = malloc(256 * sizeof(char));
 
-        token = strtok(buffer, sep1); //word 1
-        wordPair[0] = token;
-        printf("%s\n", wordPair[0]);
-        while(numWords > 1) {
-            token = strtok(NULL, sep2); //word 2
-            wordPair[1] = token;
-            printf("%s\n", wordPair[1]);
-            numWords--;
-        }
-        
-        printf("%s,%s\n", wordPair[0], wordPair[1]);
+        sscanf(buffer, "%s", joinedWords);
+
+        token = strtok(joinedWords, sep1);
+        word_1 = token;
+        token = strtok(NULL, sep1);
+        word_2 = token;
+
+        wordPair[0] = word_1;
+        wordPair[1] = word_2;
+
+        controller->c_wordList->numWords += 1;
         saveWords(wordPair, controller->c_wordList);
     }
 }
 
 void saveWords(void *wPair, WordList *list) {
-    printf("saving word...%d\n", list->numWords);
-    size_t size = (size_t) list->numWords + 1;
+    size_t size = (size_t) list->numWords;
     size_t char_size = (size_t) (2 * sizeof(char*));
     void **re_wordList = realloc(list->words, size * char_size);
 
     list->words = re_wordList;
-    list->words[list->numWords] = wPair;
-    list->numWords += 1;
-    
+    list->words[list->numWords - 1] = wPair;
 }
